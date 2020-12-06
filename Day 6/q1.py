de1bb13db0a09405b9e92ad22e2001c85f25f9ba
KyleLamoureux <kylelolemail@gmail.com>
2020-12-05 23:20:35 -0600

    Day 6 done.

---
+++ Day 6/q1.py
@@ -0,0 +1,13 @@
+puzzle = open('input.txt').read().strip().split('\n\n')
+
+total = 0
+anwnser = set()
+
+for line in puzzle:
+    for group in line.split("\n"):
+        for x in group:
+            anwnser.add(x)
+    total += len(anwnser)
+    anwnser = set()
+
+print("total: ", total)
