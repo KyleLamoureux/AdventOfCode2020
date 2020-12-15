f64287f4222d6a82fb89a6c296266b79783d656e
KyleLamoureux <kylelolemail@gmail.com>
2020-12-15 13:43:13 -0600

    Exams done. Now day 7 done.

--- Day 7/q2.py
+++ Day 7/q2.py
@@ -9,37 +9,18 @@ for line in puzzle:
     graph[key.strip().replace('bags', 'bag')] = [(con[3:].replace('bags', 'bag').replace('.', '').strip(), int(con[:3].strip())) for con in connections.split(',') if 'no other bags' not in con]
 
 count = 0
-test = 0
 
-def contain_bag(key, many, v):
-    global test
-
-    total = 0
-    if key not in graph.keys():
-        print("fuck")
-        return 0
-    elif len(graph[key]) == 0:
-        test += 1*many
-        return 1*many
+def contain_bag(key):
+    total = 1
+    if len(graph[key]) == 0:
+        return total
     else:
-        level = 0
         for sub_key in graph[key]:
-            print(graph[key])
-            print("-- ", sub_key[0], sub_key[1]) 
-            total = contain_bag(sub_key[0], sub_key[1], v+1)*many
-            if v != 0:
-                test = test + total
-            # + v
-            # total *= graph[key][0][1]
-            print("- ", total, "-- ", v)
-            print("test: " , test)
+            total += sub_key[1] * contain_bag(sub_key[0])
         return total
 
-# for k in graph.keys():
-#     if k != FIND:
-#         count = count+1 if contain_bag(k) > 0 else count
+count = contain_bag(FIND)
 
-print(graph)
-count = contain_bag(FIND, 1, 0)
+start = graph[FIND]
 
-print("Total: ", count)
+print("Total: ", count-1)
